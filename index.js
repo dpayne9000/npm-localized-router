aea2527c1f3f153f89b93315cd42972829a6a633
Daniel Payne <daniel@havenagency.com>
2016-11-11 13:36:03 -0800

    update for callback heck

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 
 exports.getLocalization = function(locale) {
 
-var PropertiesReader = require('properties-reader');
+
 
 var fs = require("fs");
 var dir = require('node-dir');
 var _ = require('underscore');
-//console.log(locale);
+var Promise = require('bluebird');
+var PropertiesReader = require('properties-reader');
+var filesPromise = Promise.promisify(dir.files);
 
     if (typeof locale == 'undefined') {
-        locale = 'en-us';//exports.config.defaultLocale;
+        locale = 'en-us';
     }
 
     var output = "";
     var directory = "locale/"+locale+"/";
-console.log(directory);
-        dir.files(directory, function(err,files) {
-        if (err) console.log(err);
-            _.each(files,function(file) {
-            console.log(file +' file loaded');
-                        var ext = file.search('.properties');
 
-                        if (ext==-1) {
-                        console.log('file '+file+' skipped');
-                        return;
-                        }
-                        try {
-                            exports.output = PropertiesReader(file);
-                        } catch (err) {
-                           console.log(err);
-                        }
+        return filesPromise(directory).then(function(files) {
+           
+            if (files[0].search('.properties')==-1) return;
 
-            });
+            return PropertiesReader(files[0]);
+        })
+        .catch(function(err){
+            console.log('catch error'+err)
         });
 
-    return exports.output;
+
 }
